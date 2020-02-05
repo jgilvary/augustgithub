d81891507d0272d41214bcfdb062e3f524020f7e
jgilvary <42068860+jgilvary@users.noreply.github.com>
2020-02-05 08:18:56 -0500

    Create hi.java

---
+++ hi.java
@@ -0,0 +1,11 @@
+
+class hello 
+{ 
+    public static void main(String args[]) 
+    { 
+        System.out.println("hi, World 1"); 
+        System.out.println("hihi World 2"); 
+        System.out.println("hi, World 3"); 
+        System.out.println("hi, World 4"); 
+    } 
+} 
