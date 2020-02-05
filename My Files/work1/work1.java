b6077266f4f1e990e42f3d7cfa03a929c591c943
=jgilvary@gmail.com <jgilvary@gmail.com>
2020-02-05 13:47:56 +0000

    c2

---
+++ My Files/work1/work1.java
@@ -0,0 +1,18 @@
+
+class work1 
+{ 
+    public static void main( String args[]) 
+    { 
+        int i = 0;
+        System.out.println("work1-" + i); 
+        i = i +1;
+        System.out.println("work2-" + i); 
+        i = i +1;
+        System.out.println("work4-" + i); 
+        i = i +1;
+        System.out.println("work5-" + i); 
+        i = i +1;
+        System.out.println("work6-" + i); 
+        
+    } 
+} 
